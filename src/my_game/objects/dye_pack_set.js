5595e2f0ea893c61414c3903bc9f08e3c96c7a75
parthgu <gparth02@hotmail.com>
2023-02-24 11:54:03 -0800

    created file

---
+++ src/my_game/objects/dye_pack_set.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import GameObjectSet from "../../engine/game_objects/game_object_set.js";
+
+class DyePackSet extends GameObjectSet {
+  constructor() {
+    super();
+  }
+}
+
+export default DyePackSet;
