0736e21d9a19cbac438428d94ea16235fd5cfe3e
chandrap <chandrap31415@gmail.com>
2023-02-24 13:02:56 -0800

    added hero with oscillate functionality

--- src/my_game/my_game.js
+++ src/my_game/my_game.js
@@ -1,24 +1,38 @@
 "use strict"; // Operate in Strict mode such that variables must be declared before used!
 
 import engine from "../engine/index.js";
+import Hero from "./objects/hero.js";
 
 class MyGame extends engine.Scene {
   constructor() {
     super();
 
+    this.kSpriteSheet = "assets/SpriteSheet.png";
+
     // The camera to view the scene
     this.mCamera = null;
+    this.mHero = null;
+  }
+
+  load() {
+    engine.texture.load(this.kSpriteSheet);
+  }
+  
+  unload() {
+    engine.texture.unload(this.kSpriteSheet);
   }
 
   init() {
     // Step A: set up the cameras
     this.mCamera = new engine.Camera(
-      vec2.fromValues(30, 27.5), // position of the camera
-      100, // width of camera
-      [0, 0, 640, 480] // viewport (orgX, orgY, width, height)
+      vec2.fromValues(100, 50), // position of the camera
+      200, // width of camera
+      [0, 0, 800, 600] // viewport (orgX, orgY, width, height)
     );
     this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
     // sets the background to gray
+
+    this.mHero = new Hero(this.kSpriteSheet);
   }
 
   // This is the draw function, make sure to setup proper drawing environment, and more
@@ -28,11 +42,15 @@ class MyGame extends engine.Scene {
     engine.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
 
     this.mCamera.setViewAndCameraMatrix();
+
+    this.mHero.draw(this.mCamera);
   }
 
   // The Update function, updates the application state. Make sure to _NOT_ draw
   // anything from this function!
-  update() {}
+  update() {
+    this.mHero.update();
+  }
 }
 
 window.onload = function () {
--- src/my_game/objects/hero.js
+++ src/my_game/objects/hero.js
@@ -6,28 +6,38 @@ class Hero extends engine.GameObject {
     constructor(spriteTexture) {
         super(null);
         this.kDelta = 0.3;
+        this.kWidth = 9; this.kHeight = 12;
+
 
         this.mRenderComponent = new engine.SpriteRenderable(spriteTexture);
         this.mRenderComponent.setColor([1, 1, 1, 0]);
-        this.mRenderComponent.getXform().setPosition(35, 50);
-        this.mRenderComponent.getXform().setSize(9, 12);
+        this.mRenderComponent.getXform().setPosition(100, 50);
+        this.mRenderComponent.getXform().setSize(this.kWidth, this.kHeight);
         this.mRenderComponent.setElementPixelPositions(0, 120, 0, 180);
+
+        this.mWidthOscillate = new engine.Oscillate(4.5, 4, 60);
+        this.mHeightOscillate = new engine.Oscillate(6, 4, 60);
+
+        this.mIsOscillating = false;
     }
 
     update() {
-        // control by WASD
-        let xform = this.getXform();
-        if (engine.input.isKeyPressed(engine.input.keys.W)) {
-            xform.incYPosBy(this.kDelta);
+        let transform = this.getXform();
+
+        if (engine.input.isKeyClicked(engine.input.keys.Q)) {
+            this.misOscillating = true;
+            this.mWidthOscillate.reStart();
+            this.mHeightOscillate.reStart();
         }
-        if (engine.input.isKeyPressed(engine.input.keys.S)) {
-            xform.incYPosBy(-this.kDelta);
-        }
-        if (engine.input.isKeyPressed(engine.input.keys.A)) {
-            xform.incXPosBy(-this.kDelta);
-        }
-        if (engine.input.isKeyPressed(engine.input.keys.D)) {
-            xform.incXPosBy(this.kDelta);
+
+        if (this.misOscillating) {
+            transform.setSize(
+                this.kWidth + this.mWidthOscillate.getNext(),
+                this.kHeight + this.mHeightOscillate.getNext()
+            );
+
+            if (this.mWidthOscillate.done())
+                this.misOscillating = false;
         }
     }
 }
