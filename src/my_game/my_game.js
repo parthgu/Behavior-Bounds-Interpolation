e6f6146178c23bcaf072ee6591c331f9947daf2f
parthgu <gparth02@hotmail.com>
2023-02-24 12:04:55 -0800

    created hero and background

--- src/my_game/my_game.js
+++ src/my_game/my_game.js
@@ -1,24 +1,51 @@
 "use strict"; // Operate in Strict mode such that variables must be declared before used!
 
 import engine from "../engine/index.js";
+import Hero from "./objects/hero.js";
+import DyePack from "./objects/dye_pack.js";
+import DyePackSet from "./objects/dye_pack_set.js";
+import Renderable from "../engine/renderables/renderable.js";
 
 class MyGame extends engine.Scene {
   constructor() {
     super();
 
+    this.kMinionSprite = "assets/SpriteSheet.png";
+    this.kBg = "assets/bg.png";
+
     // The camera to view the scene
     this.mCamera = null;
+    this.mBg = null;
+    this.mHero = null;
   }
 
   init() {
     // Step A: set up the cameras
     this.mCamera = new engine.Camera(
       vec2.fromValues(30, 27.5), // position of the camera
-      100, // width of camera
-      [0, 0, 640, 480] // viewport (orgX, orgY, width, height)
+      200, // width of camera
+      [0, 0, 800, 600] // viewport (orgX, orgY, width, height)
     );
     this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
     // sets the background to gray
+
+    this.mBg = new engine.TextureRenderable(this.kBg);
+    this.mBg.getXform().setSize(300, 150);
+    this.mBg.getXform().setPosition(30, 27.5);
+
+    this.mHero = new Hero(this.kMinionSprite);
+    this.mHero.getXform().setSize(9, 12);
+    this.mHero.getXform().setPosition(30, 27.5);
+  }
+
+  load() {
+    engine.texture.load(this.kBg);
+    engine.texture.load(this.kMinionSprite);
+  }
+
+  unload() {
+    engine.texture.unload(this.kBg);
+    engine.texture.unload(this.kMinionSprite);
   }
 
   // This is the draw function, make sure to setup proper drawing environment, and more
@@ -28,11 +55,17 @@ class MyGame extends engine.Scene {
     engine.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
 
     this.mCamera.setViewAndCameraMatrix();
+
+    this.mBg.draw(this.mCamera);
+
+    this.mHero.draw(this.mCamera);
   }
 
   // The Update function, updates the application state. Make sure to _NOT_ draw
   // anything from this function!
-  update() {}
+  update() {
+    this.mHero.update();
+  }
 }
 
 window.onload = function () {
