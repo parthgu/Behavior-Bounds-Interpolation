b6f7bd92188f785ecd855e57bc511cce5c5e97d4
chandrap <chandrap31415@gmail.com>
2023-02-24 18:06:13 -0800

    updated speed and direction of head through gameobject

--- src/my_game/objects/head.js
+++ src/my_game/objects/head.js
@@ -3,45 +3,30 @@
 import GameObject from "../../engine/game_objects/game_object.js";
 import engine from "../../engine/index.js";
 
+const kMinSpeed = 5;
+const kMaxSpeed = 10;
+
 class Head extends engine.GameObject {
   constructor() {
     super(null);
+
+    this.setSpeed(
+      (kMinSpeed + (Math.random() * (kMaxSpeed - kMinSpeed))) / 60
+    );
+    this.setCurrentFrontDir(vec2.fromValues(
+      Math.random() - 0.5,
+      Math.random() - 0.5
+    ));
+
     this.kWidth = 7.5;
     this.kHeight = 7.5;
-    this.kXPos = 100;
-    this.kYPos = 50;
-
-    this.mXSpeed = Math.floor(Math.random() * 1.083 + 0.083);
-    this.mYSpeed = Math.floor(Math.random() * 1.083 + 0.083);
-
-    if (Math.random() > 0.5) {
-      this.mXSpeed *= -1;
-    }
-    if (Math.random() > 0.5) {
-      this.mYSpeed *= -1;
-    }
 
     this.mRenderComponent = new engine.SpriteRenderable(
       "assets/SpriteSheet.png"
     );
     this.mRenderComponent.setColor([1, 1, 1, 0]);
-    // this.mRenderComponent.getXform().setPosition(this.kXPos, this.kYPos);
     this.mRenderComponent.getXform().setSize(this.kWidth, this.kHeight);
     this.mRenderComponent.setElementPixelPositions(130, 310, 0, 180);
-  }
-
-  update() {
-    // this.kXPos += this.mXSpeed;
-    // this.kYPos += this.mYSpeed;
-    // this.mRenderComponent.getXform().setPosition(this.kXPos, this.kYPos);
-  }
-
-  getSpeed() {
-    return this.mSpeed;
-  }
-
-  setSpeed(delta) {
-    this.mSpeed = delta;
   }
 }
 
